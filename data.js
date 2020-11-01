1c2df1ed0234bf0b1ee1319445904b376a198fb4
matty-lee <matthew.b.lee2@gmail.com>
2020-11-01 12:04:04 -0800

    initial commit

---
+++ data.js
@@ -0,0 +1,36 @@
+const sublinks = [
+  {
+    category: 'Shop',
+    links: [
+      { label: 'bandanas', icon: 'fas fa-credit-card', url: '#' },
+      { label: 'collars', icon: 'fas fa-credit-card', url: '#' },
+      { label: 'leashes', icon: 'fas fa-credit-card', url: '#' },
+    ],
+  },
+  {
+    category: 'Gear Buy Guide',
+    links: [
+      { label: 'leash buying guide', icon: 'fas fa-book', url: '#' },
+      { label: 'collar buying guide', icon: 'fas fa-book', url: '#' },
+    ],
+  },
+  {
+    category: 'About Us',
+    links: [
+      { label: 'meet the maker', icon: 'fas fa-briefcase', url: '#' },
+      { label: 'sizing', icon: 'fas fa-briefcase', url: '#' },
+      { label: 'shipping + return policy', icon: 'fas fa-briefcase', url: '#' },
+      { label: 'faqs', icon: 'fas fa-briefcase', url: '#' },
+    ],
+  },
+  {
+    category: 'Giving Back',
+    links: [
+      { label: 'paw for paw', icon: 'fas fa-briefcase', url: '#' },
+      { label: 'better earth program', icon: 'fas fa-briefcase', url: '#' },
+      { label: 're-home your ropes', icon: 'fas fa-briefcase', url: '#' },
+    ],
+  },
+];
+
+export default sublinks;
