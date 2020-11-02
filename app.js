06f9b3485ec7fb5b57843089dc97505108f073a8
matty-lee <matthew.b.lee2@gmail.com>
2020-11-02 13:11:57 -0800

    pet gear javascript

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-import './sublinks.js'
-import './banner.js'
+import './sublinks.js';
+import './banner.js';
 
+const gearSquare = [...document.querySelectorAll('.gear-square')];
+
+gearSquare.forEach((square) => {
+  square.addEventListener('mouseover', () => {
+    square.firstElementChild.style = 'transform: scale(1.1)';
+  });
+  square.addEventListener('mouseout', () => {
+     square.firstElementChild.style = 'transform: scale(1)';
+  });
+});
